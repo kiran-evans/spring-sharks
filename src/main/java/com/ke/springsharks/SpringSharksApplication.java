53f53611e0e7334114d9d216336156a2ee371e7a
kiran-evans <thekiranevans@gmail.com>
2022-03-08 14:20:46 +0000

    Initial commit

---
+++ src/main/java/com/ke/springsharks/SpringSharksApplication.java
@@ -0,0 +1,13 @@
+package com.ke.springsharks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSharksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSharksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ke/springsharks/SpringSharksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ke.springsharks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSharksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
